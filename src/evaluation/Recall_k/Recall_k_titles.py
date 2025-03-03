a3521434fd11bdeec51f4d9181e4ebc2fe56b408
Yurii Pozhidaev <y.pozhidaev@g.nsu.ru>
2025-03-03 15:15:39 +0700

    done

--- src/evaluation/Recall_k/Recall_k_titles.py
+++ src/evaluation/Recall_k/Recall_k_titles.py
@@ -1,17 +1,25 @@
 import json
-
+import pandas as pd
 class RecallKTitles:
-    def __init__(self, k, recs_path, model):
+    def __init__(self, k, recs_path, model, dataset_path):
         self.k = k
         self.recs_path = recs_path
         self.model = model
-    
+        self.data = pd.read_csv(dataset_path)
+
+    def intersections_with_all_books(self, list2):
+        counter = 0
+        for book in list2:
+            if book in self.data['Title'].str.lower().values:
+                counter += 1
+        return counter
+
     def compare_lists(self, list1, list2):
-        list1 = list(map(str.lower, list1))
+        list1 = list(map(lambda x: str.lower(x[0]), list1))
         list2 = list(map(lambda x: str.lower(x[0]), list2))
         intersection = set(list1) & set(list2)  
         count = len(intersection)
-        counter = intersections_with_all_books(list2)
+        counter = self.intersections_with_all_books(list1)
 
         positions = []
         for item in intersection:
@@ -22,30 +30,28 @@ class RecallKTitles:
         return {
             "intersect_count": count,
             "intersecting_items": positions,
-            "books_in_dataset": all
+            "books_in_dataset": counter
         }
 
-    def intersections_with_all_books(self, list2):
-        counter = 0
-        for book in list2:
-            if (df['Title'].str.lower() == book):
-                counter += 1
-        return counter
 
     def evaluate(self):
-        with open("./computed.json", "r", encoding="utf-8") as f:
+        with open(self.recs_path, "r", encoding="utf-8") as f:
             data = json.load(f)
 
         for entry in data:
             for book_info, recommendations in entry.items():
-                title, author, genres = eval(book_info)
+                try:
+                    title, author, genres = eval(book_info)
 
-                predicted = self.model.recommend_by_title(title, n=self.k)
-                comparison_res = self.compare_lists(recommendations, predicted)
-                print(f"For book {title} found {comparison_res['intersect_count']} intersections with n = {self.k}, dataset have = {comparison_res['books_in_dataset']}")
+                    predicted = self.model.recommend_by_title(title, n=self.k)
+                    comparison_res = self.compare_lists(recommendations, predicted)
+                    print(f"For book {title} found {comparison_res['intersect_count']} intersections with n = {self.k}, dataset have = {comparison_res['books_in_dataset']}")
 
-                comparison_res = self.compare_lists(recommendations[:10], predicted)
-                print(
-                    f"For book {title} found {comparison_res['intersect_count']} intersections with 10 best recommendations and n = {self.k}, dataset have = {comparison_res['books_in_dataset']}")
+                    comparison_res = self.compare_lists(recommendations[:10], predicted)
+                    print(
+                        f"For book {title} found {comparison_res['intersect_count']} intersections with 10 best recommendations and n = {self.k}, dataset have = {comparison_res['books_in_dataset']}")
 
-                print("\n" + "-" * 50 + "\n")
+                    print("\n" + "-" * 50 + "\n")
+
+                except ValueError:
+                    print(f"{title} not found")
