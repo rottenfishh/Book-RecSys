14ef0dcc1b641761494a89a8fdcbec6c625d525e
Ivan Shebanov <i.shebanov@g.nsu.ru>
2025-03-12 19:28:30 +0700

    Added folders in experiments

--- experiments/evaluation/evaluations.py
+++ experiments/evaluation/evaluations.py
@@ -3,12 +3,12 @@ import os
 from src.evaluation import RecallKTitles
 from src.models.modules import BookDescriptionEmbeddingSimilarity
 
-PROJECT_ROOT = os.path.abspath(os.path.join(os.getcwd(), ".."))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.getcwd(), "..", ".."))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-    from src.evaluation import RecallKTitles
-    from src.models.modules import BookDescriptionEmbeddingSimilarity
+from src.evaluation import RecallKTitles
+from src.models.modules import BookDescriptionEmbeddingSimilarity
 
 k = 1000
 recs_path = os.path.join(PROJECT_ROOT, "data/test_data/tests.json")
